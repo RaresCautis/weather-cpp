3403f2c551098f154cf96089a3de9e5543a1dc02
RaresCautis <najenunta@yahoo.com>
2025-02-25 15:52:41 +0000

    Improve location select prototype

--- src/main.cpp
+++ src/main.cpp
@@ -23,30 +23,41 @@ int main() {
     auto vfs = 0;
 
     std::vector<std::string> entries = {
-        "HELLO", "HELLO", "HELLO", "HELLO", "HELLO", "HELLO", "HELLO", "HELLO",
+        "HELLO", "HELLO", "\U0001F1EC\U0001F1E7", "HELLO", "HELLO", "HELLO",
+        "HELLO", "HELLO",
     };
 
     auto btssselect = 0;
     auto mmmm = Menu(&entries, &btssselect);
     auto btsss = Renderer(mmmm, [&mmmm] {
         return mmmm->Render() | vscroll_indicator | yframe |
-               size(ftxui::HEIGHT, ftxui::LESS_THAN, 3);
+               size(ftxui::HEIGHT, ftxui::EQUAL, 3);
     });
 
-    auto inasdt = Container::Vertical({Input() | CatchEvent([&vfs](Event e) {
-                                           if (e == Event::ArrowDown ||
-                                               e == Event::ArrowUp)
-                                               return true;
-                                           if (e == Event::Return) {
-                                               vfs = 1;
-                                               return true;
-                                           }
-                                           return false;
-                                       })});
+    int counterr = 0;
+
+    auto inasdt = Container::Vertical(
+        {Input() | CatchEvent([&vfs, &counterr, &entries](Event e) {
+             if (e.is_character()) {
+                 counterr++;
+                 entries[1] = std::to_string(counterr);
+             }
+             if (e == Event::ArrowDown || e == Event::ArrowUp)
+                 return true;
+             if (e == Event::Return) {
+                 vfs = 1;
+                 return true;
+             }
+             return false;
+         })});
+
+    auto innn = Renderer(inasdt, [&inasdt] {
+        return inasdt->Render() | size(ftxui::WIDTH, ftxui::EQUAL, 30);
+    });
 
     auto bts = Container::Vertical(
         {
-            inasdt,
+            innn,
             btsss,
         },
         &vfs);
@@ -55,36 +66,26 @@ int main() {
         Container::Horizontal({
             Renderer(bts,
                      [&bts] {
-                         auto box = vbox({
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                        text("WORLD"),
-                                    }) |
-                                    size(ftxui::HEIGHT, ftxui::EQUAL, 1) |
-                                    frame;
-
-                         return vbox({filler(), window(text(""), bts->Render()),
+                         return vbox({filler(), bts->Render() | clear_under,
                                       filler()}) |
-                                center;
+                                center | bgcolor(Color::Black) | border;
                      }),
         }) |
         center;
 
-    auto tabtab = Maybe(tr, &tabtabsel);
+    auto tabtab =
+        Maybe(Renderer(tr, [&tr] { return tr->Render(); }), &tabtabsel);
 
     initializeConfig();
 
+    bool dimm = false;
+
     auto mainMenu = MenuComponent({{"Do Nothing", [&] { currentTab = 1; }},
                                    {"Exit", [&screen] { screen.Exit(); }},
-                                   {"HAHA", [&tabtabsel, &tabtab] {
+                                   {"HAHA", [&tabtabsel, &tabtab, &dimm] {
                                         tabtabsel = 1;
                                         tabtab->TakeFocus();
+                                        dimm = true;
                                     }}});
     std::string ascii_art = R"(
                 |
@@ -127,6 +128,9 @@ int main() {
             aux = ascii_art;
             ascii_art = ascii_art2;
             ascii_art2 = aux;
+
+            if (!entries.empty())
+                entries.pop_back();
 
             screen.PostEvent(Event::Custom);
 
@@ -171,8 +175,13 @@ int main() {
 
     mainWindow.setChild(pages);
     mainComponent = mainWindow.getComponent();
+    auto mainComponentt = Renderer(mainComponent, [&mainComponent, &dimm] {
+        if (!dimm)
+            return mainComponent->Render();
+        return mainComponent->Render() | dim;
+    });
 
-    auto xd = Container::Stacked({tabtab, mainComponent});
+    auto xd = Container::Stacked({tabtab, mainComponentt}); // fix this
     xd->SetActiveChild(mainComponent);
     mainComponent->TakeFocus();
 
